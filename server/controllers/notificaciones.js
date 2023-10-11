c0e787aee782eb092087759aa1c6e9426cc1e37a
Alejandro <alexvidalcasado@gmail.com>
2023-10-11 17:01:58 +0200

    comento

--- server/controllers/notificaciones.js
+++ server/controllers/notificaciones.js
@@ -3,25 +3,24 @@ const notificacionesPost = (req, res) => {
   const noti = req.body;
   if (noti.event === "send_order" && noti.status === 200) {
     // TODO: manejar los pedidos
+    // recojemos la informacion de los pedidos
     const { data } = noti;
-
     const { items, payments } = data;
-
+    // montamos el mensaje a enviar
     const message = {
       id: data.orderId,
       items,
       payed: payments.length > 0,
     };
-
+    // si el pedido esta pagado, añadimos el total y la propina
     if (message.payed) {
       message.total = payments[0].amount;
       message.tip = payments[0].tip;
     }
-
+    // TODO: eliminar el console.log, son solo para desarrollo
     console.log({ data, items, payments });
-
     console.log({ message });
-
+    // enviamos el mensaje a la tienda por mqtt
     enviarPedido("tienda1", JSON.stringify(message));
   }
 
