6340722f0cf9ec9a2042611c9d41b0f67b1098db
Alejandro <alexvidalcasado@gmail.com>
2023-12-04 17:46:21 +0100

    sasadasdsada

--- server/controllers/notificaciones.js
+++ server/controllers/notificaciones.js
@@ -31,23 +31,20 @@ const notificacionesPost = (req, res) => {
     let ticket = `
     [magnify: width 2; height 2]
     [column: left HONEI; right Hora ${date.getHours()}:${date.getMinutes()}]
-    [column: left - Large; right * 1 \[ \]; indent 60]
     `;
 
     console.log(items);
     items.forEach((item) => {
       if (item.modifiers) {
         console.log(items.modifiers);
-        ticket += `[column: left > ${item.name}; right * ${item.quantity} \[ \]]`;
+        ticket += `[column: left > ${item.name}   ; right * ${item.quantity} \[ \]]`;
         item.modifiers.forEach((mod) => {
-          ticket += `[column: left - ${mod.name}; right * ${mod.quantity} \[ \]; indent 60]`;
+          ticket += `[column: left - ${mod.name}  ; right *${mod.quantity} \[ \]]`;
         });
       } else {
-        ticket += `[column: left > ${item.name}; right * ${item.quantity} \[ \]]`;
+        ticket += `[column: left > ${item.name}   ; right * ${item.quantity} \[ \]]`;
       }
     });
-
-    ticket += "[cut: feed; partial]";
 
     recHit(
       "fac_demo",
